dbbdf47122797ee18fdda3131d3dfabbf77bdaa1
teknoinf <113332942+teknoinf@users.noreply.github.com>
2022-09-12 12:47:13 +0530

    Update A.java

--- src/test/java/Test/A.java
+++ src/test/java/Test/A.java
@@ -10,5 +10,6 @@ public static void main(String[] args) {
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.amazon.com/");
+	system.out.println("neha");
 }
 }
